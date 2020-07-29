c7c1f0420163d1ff189758e141120765a105f297
ItsOpaz <olli.pettersson@outlook.com>
2020-07-29 14:25:08 +0300

    middleware modification and build

---
+++ build/precache-manifest.4d2ca63fed33a8306686b628bc0fd368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a04f8f22145472b0f68e21dd705a31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cc5e402acf6c670ef1a",
+    "url": "/static/css/main.c6d5ae2f.chunk.css"
+  },
+  {
+    "revision": "bade6badc4ee71057448",
+    "url": "/static/js/2.483051de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.483051de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cc5e402acf6c670ef1a",
+    "url": "/static/js/main.c5587a8b.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
