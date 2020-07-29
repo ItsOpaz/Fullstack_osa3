6540f9b3f136df1ee6bbf3738e21a15efa581719
ItsOpaz <olli.pettersson@outlook.com>
2020-07-29 14:17:18 +0300

    changes for heroku

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
+
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms  :body'));
 
@@ -96,7 +99,7 @@ let persons = [
     response.json(person)
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
