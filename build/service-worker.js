4f9804a493eb5f24762c5f41020888fa466245c6
ItsOpaz <olli.pettersson@outlook.com>
2020-07-29 14:27:57 +0300

    korajttu build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d2ca63fed33a8306686b628bc0fd368.js"
+  "/precache-manifest.302f654ef303a82327aa1a67d1517330.js"
 );
 
 self.addEventListener('message', (event) => {
